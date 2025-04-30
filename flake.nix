6e26f959de9bf5235874017a37a9da9109c3a6f3
Samuel Sacco <samuelsacco@Samuels-MacBook-Air.local>
2025-04-30 15:26:20 -0400

    Add mac-app-util for better macOS integration

--- flake.nix
+++ flake.nix
@@ -1,15 +1,19 @@
 {
   description = "Basic Mac Setup";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    mac-app-util.url = "github:hraban/mac-app-util";
   };
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, mac-app-util, ... }:
   {
     darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
       modules = [
         ./configuration.nix
+        mac-app-util.darwinModules.default
       ];
     };
   };
